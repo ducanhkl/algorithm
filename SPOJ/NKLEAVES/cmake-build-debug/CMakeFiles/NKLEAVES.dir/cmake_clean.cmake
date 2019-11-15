90560ba951946ecbed3e5460eb05cfbf46601a69
chuducanh19991310@gmail.com <chuducanh19991310@gmail.com>
2019-11-15 12:17:11 +0700

    15/11/2019

---
+++ SPOJ/NKLEAVES/cmake-build-debug/CMakeFiles/NKLEAVES.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NKLEAVES.dir/main.cpp.o"
+  "NKLEAVES"
+  "NKLEAVES.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NKLEAVES.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
