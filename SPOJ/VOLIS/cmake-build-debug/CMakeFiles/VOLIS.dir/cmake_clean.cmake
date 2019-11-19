d6b8df54ca862e94a1cdb2500a75c58978fbe4e6
Chu Duc Anh <chuducanh19991310@gmail.com>
2019-11-19 20:50:57 +0700

    asdf

---
+++ SPOJ/VOLIS/cmake-build-debug/CMakeFiles/VOLIS.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VOLIS.dir/main.cpp.obj"
+  "VOLIS.exe"
+  "VOLIS.exe.manifest"
+  "VOLIS.pdb"
+  "libVOLIS.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VOLIS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ SPOJ/VOLIS/main.cpp
@@ -0,0 +1,47 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+const int maxn = 1e3+100;
+const long long maxc = 1e9+1e5;
+
+int n, d;
+long long a[maxn], last[maxn];
+
+int main()
+{
+    //freopen("inp.txt", "r", stdin);
+    ios_base::sync_with_stdio(0);
+    cin >> n >> d;
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+    for (int i = 1; i <= n; i++)
+    {
+        last[i] = maxc;
+    }
+    last[0] = -maxc;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = i; j >= 1; j --)
+        {
+            if (a[i]-d >= last[j-1])
+            {
+                last[j] = min(last[j], a[i] - d);
+            } else
+            {
+                if (a[i] + d >= last[j-1])
+                {
+                    last[j] = min(last[j], last[j-1]);
+                }
+            }
+        }
+    }
+    for (int i = n; i >= 1; i--)
+    {
+        if (last[i] != maxc)
+        {
+            cout << i;
+            return 0;
+        }
+    }
+}
